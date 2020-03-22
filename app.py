318bd497668e6f2691c4c55e0c687cdbdc896b60
Joacko <joaquinbf@live.com>
2020-03-22 07:53:06 -0300

    listo para testing

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from flask import redirect
 import op
 import json
 
+db = None
+
+with open("db.json", "r") as db_file:
+    db = json.load(db_file)
+
 # Se crea un servidor
 app = Flask(__name__)
 
@@ -13,15 +18,13 @@ app = Flask(__name__)
 # ruta index
 @app.route('/')
 def index():
-    return render_template('index.html')
+    productos = db["productos"]
+    return render_template('index.html', productos=productos)
 
-# ruta productos
-@app.route('/')
-def productos():
-    productos = json.loads('db.json')
-    return productos
+
 
 # Se inicia el servidor en el puerto 4000 del localhost
 if __name__ == "__main__":
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True, port=4000)
-
